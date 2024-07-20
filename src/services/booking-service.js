ca3debdf39463f778119085cf780611e7f2b5a36
KeshavTandon <keshavtandon51@gmail.com>
2024-07-20 20:21:42 +0530

    Added idempotency logic in payment method

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -9,22 +9,18 @@ const {Enum} =require('../utils/common')
 const bookingRepository=new BookingRepository();
 
 async function createBooking(data) {
-  // console.log(data);
   const transaction = await db.sequelize.transaction();
   try {
     const flight = await axios.get(
       `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`
     );
     const flightData = flight.data.data;
-    console.log(flightData);
     if (data.noOfSeats > flightData.totalSeats) {
       throw new AppError("Not enough seats available", StatusCodes.BAD_REQUEST);
     }
-    console.log(data.noOfSeats);
     typeof(noOfSeats)
     const totalBillingAmount = data.noOfSeats * flightData.price;
     const bookingPayload = { ...data, totalCost: totalBillingAmount };
-    console.log(bookingPayload);
     const booking = await bookingRepository.create(bookingPayload, transaction);
 
      await axios.patch(
@@ -43,7 +39,6 @@ async function createBooking(data) {
 }
 
 async function makePayment(data) {
-  console.log(data);
   const transaction = await db.sequelize.transaction();
   try {
     const bookingDetails = await bookingRepository.get(
@@ -88,7 +83,6 @@ async function cancelBooking(bookingId) {
   const transaction = await db.sequelize.transaction();
   try {
     const bookingDetails = await bookingRepository.get(bookingId, transaction);
-    console.log(bookingDetails);
     if (bookingDetails.status == Enum.BOOKING_STATUS.CANCELLED) {
       await transaction.commit();
       return true;
@@ -114,7 +108,6 @@ async function cancelBooking(bookingId) {
 
 async function cancelOldBookings() {
   try {
-    console.log("Inside service");
     const time = new Date(Date.now() - 1000 * 300); // time 5 mins ago
     const response = await bookingRepository.cancelOldBookings(time);
 
